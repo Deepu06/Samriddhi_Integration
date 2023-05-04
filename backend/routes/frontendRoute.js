861bbddc7dd6010670e5f03d946902f38df27013
Deepu M <deem19cs@cmrit.ac.in>
2023-05-04 17:39:34 +0530

    integrated sales page to dashboard

--- backend/routes/frontendRoute.js
+++ backend/routes/frontendRoute.js
@@ -66,11 +66,11 @@ router.route("/adminBuyerDashboard").get((req, res) => {
     res.sendFile(path)
 })
 
-router.route("/BuyerAdminDashboardMembers").get((req, res) => {
-    let path = process.cwd()
-    path = Path.join(path, "frontend", "Admin", "Buyer", "Members.html")
-    res.sendFile(path)
-})
+// router.route("/BuyerAdminDashboardMembers").get((req, res) => {
+//     let path = process.cwd()
+//     path = Path.join(path, "frontend", "Admin", "Buyer", "Members.html")
+//     res.sendFile(path)
+// })
 router.route("/BuyerAdminDashboardAddMember").get((req, res) => {
     let path = process.cwd()
     path = Path.join(path, "frontend", "Admin", "Buyer", "AddMember.html")
@@ -123,11 +123,11 @@ router.route("/adminSellerDashboard").get((req, res) => {
 })
 
 //Selling Admin Dashboard pages routing
-router.route("/SellingAdminDashboardMembers").get((req, res) => {
-    let path = process.cwd()
-    path = Path.join(path, "frontend", "Admin", "Seller", "Members.html")
-    res.sendFile(path)
-})
+// router.route("/SellingAdminDashboardMembers").get((req, res) => {
+//     let path = process.cwd()
+//     path = Path.join(path, "frontend", "Admin", "Seller", "Members.html")
+//     res.sendFile(path)
+// })
 router.route("/SellingAdminDashboardAddMember").get((req, res) => {
     let path = process.cwd()
     path = Path.join(path, "frontend", "Admin", "Seller", "AddMember.html")
